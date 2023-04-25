b9ff68bdbdf5ecd4cea4d93ec48ca7ee8eae25b9
Nausheen Anwar <nausheen.anwar@pb.com>
2023-04-25 23:33:23 +0530

    course application

---
+++ course-api/src/main/java/TestingFormat.java
@@ -0,0 +1,10 @@
+
+public class TestingFormat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(String.format("%.2f",(double)(88988.8888/874359)));
+
+	}
+
+}
